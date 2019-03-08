97cdd0fcd0c43060de78ddd3c163bae2b61f5652
Ekaterina Ryazanova <katyacoil7@gmail.com>
2019-03-08 18:38:48 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,252 @@
+const canvas = document.querySelector('#canvas');
+console.log(canvas);
+
+var ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'rgb(0, 50, 0)';
+
+ctx.beginPath();
+ctx.moveTo(135, 35);
+ctx.lineTo(135, 115);
+ctx.lineTo(215, 115);
+ctx.lineTo(215, 35);
+ctx.lineTo(135, 35);
+ctx.fill();
+
+ctx.fillStyle = 'rgb(0, 50, 0)';
+
+ctx.beginPath();
+ctx.moveTo(275, 35);
+ctx.lineTo(275, 115);
+ctx.lineTo(355, 115);
+ctx.lineTo(355, 35);
+ctx.lineTo(275, 35);
+ctx.fill();
+
+ctx.fillStyle = 'rgb(0, 50, 0)';
+
+ctx.beginPath();
+ctx.moveTo(215, 115);
+ctx.lineTo(215, 160);
+ctx.lineTo(170, 160);
+ctx.lineTo(170, 280);
+ctx.lineTo(210, 280);
+ctx.lineTo(210, 250);
+ctx.lineTo(280, 250);
+ctx.lineTo(280, 280);
+ctx.lineTo(320, 280);
+ctx.lineTo(320, 160);
+ctx.lineTo(275, 160);
+ctx.lineTo(275, 115);
+ctx.lineTo(215, 115);
+ctx.fill();
+
+ctx.fillStyle = 'rgb(14, 190, 32)';
+
+ctx.beginPath();
+ctx.moveTo(355, 80);
+ctx.lineTo(355, 115);
+ctx.lineTo(275, 115);
+ctx.lineTo(275, 160);
+ctx.lineTo(400, 160);
+ctx.lineTo(400, 80);
+ctx.lineTo(355, 80);
+ctx.fill();
+
+ctx.fillStyle = 'rgb(194, 235, 201)';
+
+ctx.beginPath();
+ctx.moveTo(80, 320);
+ctx.lineTo(160, 320);
+ctx.lineTo(160, 160);
+ctx.lineTo(80, 160);
+ctx.lineTo(80, 320);
+ctx.fill();
+
+ctx.fillStyle = 'rgb(152, 239, 163)';
+
+ctx.beginPath();
+ctx.moveTo(160, 115);
+ctx.lineTo(160, 250);
+ctx.lineTo(170, 250);
+ctx.lineTo(170, 160);
+ctx.lineTo(215, 160);
+ctx.lineTo(215, 115);
+ctx.lineTo(160, 115);
+ctx.fill();
+
+ctx.fillStyle = 'rgb(152, 239, 163)';
+
+ctx.beginPath();
+ctx.moveTo(160, 0);
+ctx.lineTo(160, 35);
+ctx.lineTo(215, 35);
+ctx.lineTo(215, 115);
+ctx.lineTo(245, 115);
+ctx.lineTo(245, 0);
+ctx.lineTo(160, 0);
+ctx.fill();
+
+ctx.fillStyle = 'rgb(0, 102, 51)';
+
+ctx.beginPath();
+ctx.moveTo(400, 0);
+ctx.lineTo(400, 80);
+ctx.lineTo(480, 80);
+ctx.lineTo(480, 0);
+ctx.lineTo(400, 0);
+ctx.fill();
+
+ctx.fillStyle = 'rgb(0, 102, 51)';
+
+ctx.beginPath();
+ctx.moveTo(400, 160);
+ctx.lineTo(400, 240);
+ctx.lineTo(480, 240);
+ctx.lineTo(480, 160);
+ctx.lineTo(400, 160);
+ctx.fill();
+
+ctx.fillStyle = 'rgb(0, 102, 51)';
+
+ctx.beginPath();
+ctx.moveTo(0, 160);
+ctx.lineTo(0, 240);
+ctx.lineTo(80, 240);
+ctx.lineTo(80, 160);
+ctx.lineTo(0, 160);
+ctx.fill();
+
+ctx.fillStyle = 'rgb(0, 132, 30)';
+
+ctx.beginPath();
+ctx.moveTo(80, 80);
+ctx.lineTo(80, 160);
+ctx.lineTo(160, 160);
+ctx.lineTo(160, 115);
+ctx.lineTo(135, 115);
+ctx.lineTo(135, 80);
+ctx.lineTo(80, 80);
+ctx.fill();
+
+ctx.fillStyle = 'rgb(0, 140, 30)';
+
+ctx.beginPath();
+ctx.moveTo(160, 250);
+ctx.lineTo(160, 320);
+ctx.lineTo(320, 320);
+ctx.lineTo(320, 280);
+ctx.lineTo(280, 280);
+ctx.lineTo(280, 250);
+ctx.lineTo(210, 250);
+ctx.lineTo(210, 280);
+ctx.lineTo(170, 280);
+ctx.lineTo(170, 250);
+ctx.lineTo(160, 250);
+ctx.fill();
+
+ctx.fillStyle = 'rgb(194, 235, 201)';
+
+ctx.beginPath();
+ctx.moveTo(320, 320);
+ctx.lineTo(400, 320);
+ctx.lineTo(400, 240);
+ctx.lineTo(320, 240);
+ctx.lineTo(320, 320);
+ctx.fill();
+
+ctx.fillStyle = 'rgb(152, 239, 163)';
+
+ctx.beginPath();
+ctx.moveTo(400, 80);
+ctx.lineTo(400, 160);
+ctx.lineTo(480, 160);
+ctx.lineTo(480, 80);
+ctx.lineTo(400, 80);
+ctx.fill();
+
+ctx.fillStyle = 'rgb(152, 239, 163)';
+
+ctx.beginPath();
+ctx.moveTo(400, 240);
+ctx.lineTo(400, 320);
+ctx.lineTo(480, 320);
+ctx.lineTo(480, 240);
+ctx.lineTo(400, 240);
+ctx.fill();
+
+ctx.fillStyle = 'rgb(113, 255, 145)';
+
+ctx.beginPath();
+ctx.moveTo(0, 0);
+ctx.lineTo(0, 80);
+ctx.lineTo(135, 80);
+ctx.lineTo(135, 35);
+ctx.lineTo(160, 35);
+ctx.lineTo(160, 0);
+ctx.lineTo(0, 0);
+ctx.fill();
+
+ctx.fillStyle = 'rgb(113, 255, 145)';
+
+ctx.beginPath();
+ctx.moveTo(235, 0);
+ctx.lineTo(235, 115);
+ctx.lineTo(275, 115);
+ctx.lineTo(275, 35);
+ctx.lineTo(355, 35);
+ctx.lineTo(355, 80);
+ctx.lineTo(400, 80);
+ctx.lineTo(400, 0);
+ctx.lineTo(235, 0);
+ctx.fill();
+
+ctx.fillStyle = 'rgb(0, 140, 30)';
+
+ctx.beginPath();
+ctx.moveTo(0, 240);
+ctx.lineTo(0, 320);
+ctx.lineTo(80, 320);
+ctx.lineTo(80, 240);
+ctx.lineTo(0, 240);
+ctx.fill();
+
+ctx.fillStyle = 'rgb(0, 140, 30)';
+
+ctx.beginPath();
+ctx.moveTo(0, 240);
+ctx.lineTo(0, 320);
+ctx.lineTo(80, 320);
+ctx.lineTo(80, 240);
+ctx.lineTo(0, 240);
+ctx.fill();
+
+ctx.fillStyle = 'rgb(0, 140, 30)';
+
+ctx.beginPath();
+ctx.moveTo(0, 240);
+ctx.lineTo(0, 320);
+ctx.lineTo(80, 320);
+ctx.lineTo(80, 240);
+ctx.lineTo(0, 240);
+ctx.fill();
+
+ctx.fillStyle = 'rgb(107, 199, 107)';
+
+ctx.beginPath();
+ctx.moveTo(0, 80);
+ctx.lineTo(0, 160);
+ctx.lineTo(80, 160);
+ctx.lineTo(80, 80);
+ctx.lineTo(0, 80);
+ctx.fill();
+
+ctx.fillStyle = 'rgb(13, 179, 30)';
+
+ctx.beginPath();
+ctx.moveTo(320, 160);
+ctx.lineTo(320, 240);
+ctx.lineTo(400, 240);
+ctx.lineTo(400, 160);
+ctx.lineTo(320, 160);
+ctx.fill();
